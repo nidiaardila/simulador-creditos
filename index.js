87022c24d685f0b481f995e1b6db91c4c350cca7
Nidia Ardila <nidiaardila17@gmail.com>
2023-01-23 01:10:57 -0300

    first commit

--- index.js
+++ index.js
@@ -25,7 +25,13 @@ function calcularCredito(credito, cuotas, interes) {
   let tcapital = 0;
   let tinteres = 0;
 
+  //formula para calcular valor de cada cuota
   mensualidad =(credito * ((Math.pow(1 + interes / 100, cuotas) * interes) / 100)) / (Math.pow(1 + interes / 100, cuotas) - 1);
+
+  document.getElementById("tbody").innerHTML = "";
+  totalCuotas = [];
+
+
 
   for (let i = 1; i <= cuotas; i++) {
     // Calcula la fecha de pago de la cuota
@@ -46,6 +52,18 @@ function calcularCredito(credito, cuotas, interes) {
     );
     totalCuotas.push(cuota);
 
+
+    // document.getElementById("tbody").innerHTML=document.getElementById("tbody").innerHTML+
+    // `<td>${numeroCuota}</td>
+    //                <td>${fecha.toDateString()}</td>
+    //                <td>${mensualidad.toFixed(2)}</td>
+    //                <td>${pagocapital.toFixed(2)}</td>
+    //                <td>${pagointeres.toFixed(2)}</td>
+    //                <td>${credito.toFixed(2)}</td>
+    //               `;
+    // let tabla = document.getElementById("tbody");
+    // tabla.append(row);
+
     let row = document.createElement("tr");
     row.innerHTML = `<td>${numeroCuota}</td>
                    <td>${fecha.toDateString()}</td>
@@ -56,6 +74,7 @@ function calcularCredito(credito, cuotas, interes) {
                   `;
     let tabla = document.getElementById("tbody");
     tabla.append(row);
+   
 
     numeroCuota++;
   }
@@ -66,7 +85,10 @@ function calcularCredito(credito, cuotas, interes) {
     tcapital += totalCuotas[i].pagocapital;
     tinteres += totalCuotas[i].pagointeres;
   }
+    
 
+  // console.log("ESTE ES")
+  // console.log(document.getElementById("tbody"))
   let totalCuota = document.getElementById("totalCuota");
   totalCuota.innerHTML = tcuota.toFixed(2);
 
@@ -75,6 +97,7 @@ function calcularCredito(credito, cuotas, interes) {
 
   let totalInteres = document.getElementById("totalInteres");
   totalInteres.innerHTML = tinteres.toFixed(2);
+
 
   // Convertir el array de objetos 'totalCuotas' a una cadena de texto JSON
   let totalCuotasJSONString = JSON.stringify(totalCuotas);
@@ -98,10 +121,36 @@ function calcularCredito(credito, cuotas, interes) {
 
 //funcion limpiar la tabla - borrar
 function limpiarTabla() {
-  let tabla = document.getElementById("tbody");
+
+  Swal.fire({
+    title: '¿Deseas eliminar la simulacion?',
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      let tabla = document.getElementById("tbody");
   tabla.innerHTML = "";
   let filaTotales = document.getElementById("filaTotales");
   filaTotales.parentNode.removeChild(filaTotales);
+      Swal.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    }
+  })
+
+  // let tabla = document.getElementById("tbody");
+  // tabla.innerHTML = "";
+  // // totalcuota.innerHTML ="";
+  // // totalCapital.innerHTML = "";
+  // // totalInteres.innerHTML = "";
+  // let filaTotales = document.getElementById("filaTotales");
+  // filaTotales.parentNode.removeChild(filaTotales);
+
   }
   
 let credito = document.getElementById("credito");
@@ -113,7 +162,7 @@ let tabla = document.getElementById("tbody");
 let nombre = document.getElementById("nombre");
 let fecha = document.getElementById("fecha");
 
-//boton simular - llamado a funcion calcularCredito
+// boton simular - llamado a funcion calcularCredito
 let simulationDone = false;
 btnSimular.addEventListener("click", () => {
   if (!simulationDone) {
@@ -121,6 +170,13 @@ btnSimular.addEventListener("click", () => {
     simulationDone = true;
   }
 });
+
+// btnSimular.addEventListener("click", () => {
+  
+//     calcularCredito(credito.value, cuotas.value, interes.value);
+    
+  
+// });
 
 // manejador de evento para el botón Borrar - llama a la función para limpiar la tabla
 document.getElementById("btnBorrar").addEventListener("click", function () {
@@ -152,3 +208,41 @@ window.addEventListener("load", () => {
   interes.value = localStorage.getItem("interes");
   // localStorage.clear();
 });
+
+  //API openweather
+
+document.addEventListener("DOMContentLoaded", main);
+
+const key = "042084c7e790ea4cb6ceadc791a3fc89";
+let city;
+
+async function getWeather() {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`);
+    const data = await response.json();
+    updateWeather(data);
+}
+
+function updateWeather(data) {
+    const temperaturaKelvin = data.main.temp;
+    const temperaturaCelsius = temperaturaKelvin - 273.15;
+    const weather = data.weather[0].main;
+    const iconUrl = getWeatherIcon(data.weather[0].icon);
+    document.getElementById("temp").innerHTML = temperaturaCelsius.toFixed(2);
+    document.getElementById("weather").innerHTML = weather;
+    document.getElementById("icon").src = iconUrl;
+}
+
+function getWeatherIcon(iconCode) {
+    return `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+}
+
+function main() {
+    const cityInput = document.getElementById("ciudad-input");
+    cityInput.addEventListener("change", function(){
+        city = this.value;
+        getWeather();
+    });
+}
+
+
+
